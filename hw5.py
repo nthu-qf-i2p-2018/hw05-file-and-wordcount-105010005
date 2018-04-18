1d179f790974891ee2beb889a0af9fdf730495ef
105010005 <gi08201114@gmail.com>
2018-04-19 00:04:18 +0800

    4/19 first

--- hw5.py
+++ hw5.py
@@ -1,52 +1,31 @@
 # -*- coding: utf-8 -*-
-import ...
-
-
+import csv
 def main(filename):
-    # read file into lines
-    lines = ...
-
-    # declare a word list
-    all_words = []
-
-    # extract all words from lines
+    f=open(filename)
+    lines=f.readlines()
+    all_words=[]
     for line in lines:
-        # split a line of text into a list words
-        # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
-        # check the format of words and append it to "all_words" list
+        words=line.split()
         for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
+            import string
+            word=word.strip(string.punctuation)
             if word:
-                # append the word to "all_words" list
-                all_words...
-
-    # compute word count from all_words
-    counter = ...
-
-    # dump to a csv file named "wordcount.csv":
-    # word,count
-    # a,12345
-    # I,23456
-    # ...
-    with open(...) as csv_file:
-        # create a csv writer from a file object (or descriptor)
-        writer = ...
-        # write table head
-        writer.writerow(['word', 'count'])
-        # write all (word, count) pair into the csv writer
-        writer.writerows(...)
-
-    # dump to a json file named "wordcount.json"
-    ...
-
-    # BONUS: dump to a pickle file named "wordcount.pkl"
-    # hint: dump the Counter object directly
-
-
+                all_words.append(word)
+    from collections import Counter
+    counter=Counter(all_words)
+    counter=Counter()
+    counter.update(all_words)
+    with open("wordcount.csv",'w')as csv_file:
+        writer=csv.writer(csv_file)
+        writer.writerow(['word','count'])
+        for vocabulary,number in counter.most_common():
+            writer.writerow([vocabulary,str(number)])
+        csv_file.close()    
+        
+    
+        
+        
+    
+        
 if __name__ == '__main__':
-    main("i_have_a_dream.txt")
+    main('i_have_a_dream.txt')
